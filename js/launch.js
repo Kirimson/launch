81182f4e07c9aac38e9aa66bb0acd52d9d1e2369
Kieran Gates <kierangates@outlook.com>
2022-04-23 20:52:17 +0100

    fix: only import base config if present

--- js/launch.js
+++ js/launch.js
@@ -332,9 +332,11 @@ define(["require", "exports", "./launchfolder", "./launchlink", "./launchquery",
             importElem.click();
             importElem.addEventListener('change', function () {
                 self.readLaunchFile(this.files[0]).then(function (importData) {
-                    // fix up the color hex problem...
-                    importData['launch']['color'] = importData['launch']['color'].replace('(hex)', '#');
-                    self.loadConfig(importData['launch']);
+                    if (importData['launch']) {
+                        // fix up the color hex problem...
+                        importData['launch']['color'] = importData['launch']['color'].replace('(hex)', '#');
+                        self.loadConfig(importData['launch']);
+                    }
                     if (blat) {
                         self.files = [];
                         self.folders = [];
